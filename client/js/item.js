5444e1b531f5d738cf4968957f663122538ab10e
cesarcruz91 <cesarcruz91@gmail.com>
2013-10-03 22:04:22 -0400

    Started linking listview items to the individual page

--- client/js/item.js
+++ client/js/item.js
@@ -1,6 +1,10 @@
 $(document).on('pagebeforeshow', '#itempage', function(event, ui){
+	
+	var item_id = localStorage.getItem('item_id');
+	localStorage.clear();
+	$('#itemcontent').empty();
 	$.ajax({
-		url : "http://localhost:3000/item",
+		url : "http://localhost:3000/item/"+item_id,
 		contentType : "application/json",
 		success : function(data){
 			$('#itemcontent').append('<div class="ui-grid-a"><div class="ui-block-a"><img src="' + data.image + '" id="itemimage" height=150px width=150px></div><div class="ui-block-b"><h4 id="itemprice">' + data.price + '</h4><button data-role="button">Buy</button><h4>Bid now!</h4><button data-role="button">Bid</button></div></div><div><h3 id="itemname">' + data.name + '</h3><h5 id="itemdesc">' + data.description + '</h5><h5 id="itemgeneral">' + data.general + '</h5><h5 id="itemyear">Year: ' + data.year + '</h5><h5 id="itemcat"><strong>' + data.category+ '</strong></h5></div>');
@@ -10,13 +14,12 @@ $(document).on('pagebeforeshow', '#itempage', function(event, ui){
 		error : function(data){
 
 		}
-
-
-
 	});
+});
 
-
-
-
-
+$(document).on('click', '#results li', function(event)
+{
+	var parameter = $(this).attr('id');
+	localStorage.setItem('item_id', parameter);
+	$.mobile.changePage('#itempage');
 });
