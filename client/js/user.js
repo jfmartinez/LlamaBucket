a28a1694c1fe9cef988ee6d6596bf3ca417c00d5
cesarcruz91 <cesarcruz91@gmail.com>
2013-10-05 19:41:22 -0400

    Dinamic placegholders have been added to the edit_user_info page

--- client/js/user.js
+++ client/js/user.js
@@ -7,10 +7,11 @@ $(document).on('click', '#sign_in_submit', function(event)
 		data : { username : $('#username').val(), password : $('#password').val()},
 		success : function(data)
 		{
-			//Store the user in localSession
+			//Store the user in localStorage
 			localStorage.setItem('first_name', data.first_name);
 			localStorage.setItem('last_name', data.last_name);
 			localStorage.setItem('email', data.email);
+      localStorage.setItem('phone', data.phone);
 			localStorage.setItem('image', data.image);
       $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+localStorage.getItem('first_name')+'</a>')
       $('#home').page();
@@ -36,4 +37,16 @@ $(document).on('pagebeforeshow', '#user_profile', function(event)
 		$('#user_content').empty();
 		$('#user_content').append('<div class="ui-grid-a"><div class="ui-block-a"><img src="http://3.bp.blogspot.com/-Fo7AHLKOH5M/T8WeqUcZFKI/AAAAAAAAFCc/cQdII_3xu7k/s320/sloth.jpg" height="150" width="175"></div><div class="ui-block-b"><h4></h4><div class="ui-grid-a"><div class="ui-block-a"><h2> Rank </h2></div><div class="ui-block-b"><h2> 3 Stars </h2></div></div></div></div><p><strong>Email: </strong>cesarcruz91@gmail.com</p><strong>Mailling Information:</strong><p>#311 Calle Ext. Los Robles</p><p>Rincon, PR 00677</p><p>United States</p>');
 	}
+});
+
+$(document).on('pagebeforeshow', '#edit_user', function(event)
+{
+  $('#first_name').attr('placeholder', localStorage.getItem('first_name'));
+  $('#last_name').attr('placeholder', localStorage.getItem('last_name'));
+  $('#email').attr('placeholder', localStorage.getItem('email'));
+});
+
+$(document).on('click', '#edit_user_save', function(event)
+{
+  console.log('Hi');
 });
