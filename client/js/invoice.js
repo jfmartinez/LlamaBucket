1584cb12263af29f15f6f150510cc6df7e18dc7f
Luis Medina <luis.medina14@upr.edu>
2013-10-05 19:38:26 -0400

    Basic invoice logic completed.

--- client/js/invoice.js
+++ client/js/invoice.js
@@ -1,25 +1,59 @@
-$(document).on('pagebeforeshow', '#invoicepage', function(event){
+//Loads a page containing the invoice
+$(document).on('pagebeforeshow', '#invoicepage', function(event, ui){
+	//var invoice_id = localStorage.getItem('invoice_id');
+	//localStorage.clear();
 
 	$.ajax({
-		url : "http://localhost:3000/invoice"+invoice_id,
-		contentType : "application/json"
+		url : "http://localhost:3000/invoice/1",
+		//url : "http://localhost:3000/invoice"+invoice_id,
+		contentType : "application/json",
 		success : function(data){
-			$('#invoicecontent').append('<ul data-role="listview" id="invoiceinfo">'+
-											'<li>Invoice Number: ' + data.invoiceId + '</li>'+
-											'<li>Buyer : ' + data.buyer + '</li>' + 
-											'<li>Seller :' + data.seller + '</li>'+
-											'<li>')
+			var invoice = $('#invoiceinfo');
+			invoice.empty();
+			invoice.append('<li>Invoice Number: ' + data.invoiceId + '</li>'+ 
+				'<li>Buyer: ' + data.buyer + '</li>' + 
+				'<li>Seller: ' + data.seller + '</li>'+ 
+				'<li>Item: ' + data.item + '</li>' + 
+				'<li>Price: ' + data.price + '</li>' + 
+				'<li>Date: ' + data.date + '</li>' + 
+				'<li>Time: ' + data.time + '</li></ul>');
+
+			invoice.listview('refresh');
+		},
+		error : function(data){
+			console.log("Invoice could not be loaded. (invoice.js)");
 		}
+	});
+});
 
+$(document).on('click', '#invoiceinfo li', function(event, ui){
+	var parameter = $(this).attr('id');
+	localStorage.setItem('invoice_id', parameter);
+	$.mobile.changePage('#invoicepage');
 
+});
+//Loads a page containing a list of invoices
+$(document).on('pagebeforeshow', '#invoicelistpage', function(event, ui){
+	$.ajax({
+		url : "http://localhost:3000/invoice",
+		contentType : "application/json",
+		success : function(data){
+			var list = $('#invoicelist');
+			list.empty();
 
-	})
+			for(var i=0; i<data.length; i++){
+				var entry = data[i];
+				list.append('<li><a href="#"><h2>Listing: ' + entry.item + '</h2>' + 
+							'<p>Seller: ' + entry.seller + '</p>' + 
+							'<p>' + entry.date + '</p>' + 
+							'<p>' + entry.time + '</p>' + 
+							'<p class="ui-li-aside">' + entry.price + '</p></a></li>');
+			}
 
-
-
-
-
-
-
-
+			list.listview('refresh');
+		},
+		error : function(data){
+			console.log("Invoice list could not be loaded. (invoice.js)");
+		}
+	});
 });
