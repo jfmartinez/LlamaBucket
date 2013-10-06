e6fd3ff5b2407f191a867f3c22e2a13214787c16
cesarcruz91 <cesarcruz91@gmail.com>
2013-10-05 22:10:12 -0400

    Working on user functionality, user can now edit his name and email

--- client/js/user.js
+++ client/js/user.js
@@ -48,5 +48,25 @@ $(document).on('pagebeforeshow', '#edit_user', function(event)
 
 $(document).on('click', '#edit_user_save', function(event)
 {
-  console.log('Hi');
+  $.ajax({
+    type : "POST",
+    url : "http://localhost:3000/update_user_info",
+    data : $('#user_info_edit').serializeArray(),
+    success : function(data)
+    {
+      //Store the user in localStorage
+      localStorage.setItem('first_name', data.first_name);
+      localStorage.setItem('last_name', data.last_name);
+      localStorage.setItem('email', data.email);
+      localStorage.setItem('phone', data.phone);
+      localStorage.setItem('image', data.image);
+      $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+localStorage.getItem('first_name')+'</a>')
+      $('#home').page();
+      $.mobile.changePage('#user_profile')
+    },
+    error : function(data)
+    {
+      console.log('no brego');
+    },
+  })
 });
