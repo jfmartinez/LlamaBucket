6e5f944a5495e984523af74150ab66d435a82425
cesarcruz91 <cesarcruz91@gmail.com>
2013-10-06 16:38:05 -0400

    Refactoring the code, there where alot of errors in the html do to merges.

--- client/js/user.js
+++ client/js/user.js
@@ -9,12 +9,12 @@ $(document).on('click', '#sign_in_submit', function(event)
 		success : function(data)
 		{
 			//Store the user in sessionStorage
-			sessionStorage.setItem('first_name', data.first_name);
-			sessionStorage.setItem('last_name', data.last_name);
-			sessionStorage.setItem('email', data.email);
-      sessionStorage.setItem('phone', data.phone);
-			sessionStorage.setItem('image', data.image);
-      $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+sessionStorage.getItem('first_name')+'</a>')
+			localStorage.setItem('first_name', data.first_name);
+			localStorage.setItem('last_name', data.last_name);
+			localStorage.setItem('email', data.email);
+      localStorage.setItem('phone', data.phone);
+			localStorage.setItem('image', data.image);
+      $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+localStorage.getItem('first_name')+'</a>')
       $('#home').page();
 			$.mobile.changePage('#home')
 		},
@@ -29,10 +29,10 @@ $(document).on('click', '#sign_in_submit', function(event)
 //Show the user information on the user page.
 $(document).on('pagebeforeshow', '#user_profile', function(event)
 {
-  $('#profile_image').attr('src',sessionStorage.getItem('image'));
-  $('#user_profile_name').html(''+sessionStorage.getItem('first_name') +' '+ sessionStorage.getItem('last_name'));
-  $('#user_profile_email').html(sessionStorage.getItem('email'));
-  $('#user_profile_phone').html(sessionStorage.getItem('phone'));
+  $('#profile_image').attr('src',localStorage.getItem('image'));
+  $('#user_profile_name').html(''+localStorage.getItem('first_name') +' '+ localStorage.getItem('last_name'));
+  $('#user_profile_email').html(localStorage.getItem('email'));
+  $('#user_profile_phone').html(localStorage.getItem('phone'));
 
 
 
@@ -41,7 +41,7 @@ $(document).on('pagebeforeshow', '#user_profile', function(event)
 //Properly redirect the user depending on his status on the page.
 $(document).on('click', '#my_profile', function(event)
 {
-  if(sessionStorage.getItem('email'))
+  if(localStorage.getItem('email'))
   {
     $.mobile.changePage('#user_profile');
   }
@@ -54,9 +54,9 @@ $(document).on('click', '#my_profile', function(event)
 //Before showing the edit user page, load the placeholders with the user data.
 $(document).on('pagebeforeshow', '#edit_user', function(event)
 {
-  $('#first_name').attr('placeholder', sessionStorage.getItem('first_name'));
-  $('#last_name').attr('placeholder', sessionStorage.getItem('last_name'));
-  $('#email').attr('placeholder', sessionStorage.getItem('email'));
+  $('#first_name').attr('placeholder', localStorage.getItem('first_name'));
+  $('#last_name').attr('placeholder', localStorage.getItem('last_name'));
+  $('#email').attr('placeholder', localStorage.getItem('email'));
 });
 
 //Edit the user credentials
@@ -69,12 +69,12 @@ $(document).on('click', '#edit_user_save', function(event)
     success : function(data)
     {
       //Store the user in sessionStorage
-      sessionStorage.setItem('first_name', data.first_name);
-      sessionStorage.setItem('last_name', data.last_name);
-      sessionStorage.setItem('email', data.email);
-      sessionStorage.setItem('phone', data.phone);
-      sessionStorage.setItem('image', data.image);
-      $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+sessionStorage.getItem('first_name')+'</a>')
+      localStorage.setItem('first_name', data.first_name);
+      localStorage.setItem('last_name', data.last_name);
+      localStorage.setItem('email', data.email);
+      localStorage.setItem('phone', data.phone);
+      localStorage.setItem('image', data.image);
+      $('#sign_in_button').replaceWith('<a href="#user_profile" data-role="button" id="my_profile_home">'+localStorage.getItem('first_name')+'</a>')
       $('#home').page();
       $.mobile.changePage('#user_profile')
     },
