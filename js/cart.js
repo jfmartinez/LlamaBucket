c1e254efaa60d4901670f042580233f6f34b1719
cesarcruz91 <cesarcruz91@gmail.com>
2013-11-09 16:48:40 -0400

    Initial implementation of the checkout page, fetches entries from the db

--- js/cart.js
+++ js/cart.js
@@ -40,65 +40,11 @@ $(document).on('pagebeforeshow', '#user_bucket', function(event){
 
 });
 
-// 
-$(document).on('pagebeforeshow', '#checkout_address', function(event){
-
-	
-	$.ajax({
-		url : "http://localhost:3000/checkout_address",
-		contentType : "application/json",
-		success : function(data)
-		{
-			var sub_total = 0;
-			var sub_total_list = $('#sub_total');
-			var sub_total_amount = $('#values');
-			var item_count = 0;
-			var length = data.content.length;
-			var total_id = $('#total_id');
-			var total = $('#total');
-
-
-
-			for(var i = 0; i < length; i++)
-			{
-
-				sub_total += data.content[i].price;
-			}
-			
-			sub_total_list.empty();
-			sub_total_amount.empty();
-
-
-			total_id.empty();
-			total.empty();
-
-			$('#user_address').empty();
-			$('#checkout_items').empty();
-			sub_total_list.append("<p style=\"font-size:12px\" id=\"hello_world\">Subtotal ( "+length+" items ): <br /> Shipping: </p>");
-			sub_total_amount.append("<p style=\"font-size:12px\">$"+ sub_total+".00 <br /> $2.00</p>");
-
-
-
-			total_id.append("<p style=\"font-size:12px\">Total</p>");
-			total.append("<p style=\"font-size:12px\">$"+ (sub_total + 2)+".00</p>");
-
-			$('#user_address').append("<p>"+data.address[0].address_one +
-			 "</p><p>" + data.address[0].address_two +
-			  "</p><p>" +data.address[0].city +
-			   ", " + data.address[0].zipcode + 
-			   "</p><p>" +data.address[0].country +"</p>");
-
-			for(var i = 0; i < length; i++)
-			{
-				$('#checkout_items').append("<li><a href=\"#\"></a><div class=\"ui-grid-b\"><div class=\"ui-block-a\"> <img src=\"" + data.content[i].image + "\" height=\"60\" width=\"60\"></div><div class=\"ui-block-b\"><h5>"+data.content[i].name + "</h5><p>" + data.content[i].description + "</p></div><div class=\"ui-block-c\"><h6 align=\"center\">Price: $"+data.content[i].price+"</h6></div></div><a href=\"#editItems\" data-rel=\"dialog\" data-position-to=\"window\" data-transition=\"pop\"></a></li>");
-			}	
-			$('#checkout_items').listview('refresh');
-		},
-		error : function(data){
-			console.log("Checkout information not available");
-		}
-	});
-
+$(document).on('click', '#bucket_checkout', function(event)
+{
+	//I can only press checkout button from the bucket, right?
+	sessionStorage.setItem('bucket_id', localStorage.getItem('id'));
+	$.mobile.changePage('#checkout');
 });
 
 
