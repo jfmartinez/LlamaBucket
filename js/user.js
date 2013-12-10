7305328502fa32ea7978d3d8e92cbc06934e2b82
cesarcruz91 <cesarcruz91@gmail.com>
2013-12-10 11:49:12 -0400

    Added basic post to register a new user, no checks are being made, data is being sent without filtering

--- js/user.js
+++ js/user.js
@@ -33,6 +33,34 @@ $(document).on('click', '#sign_in_submit', function(event)
   })
 });
 
+$(document).on('click', '#register_user', function (event) {
+
+  $.ajax({
+    
+    type : "POST",
+
+    url : "http://" + lb_server + "/register_user",
+
+    data : $('#new_user').serializeArray(),
+
+    success : function (data) {
+
+      console.log('User has been signed in.');
+
+      //Store the user in localStorage
+      localStorage.setItem('id', data.id);
+
+      $.mobile.changePage('#user_profile');
+    },
+
+    error : function (data) {
+      console.log(data);
+
+      $.mobile.changePage('#home')
+    }
+  });
+});
+
 
 $(document).on('pagebeforeshow', '#home', function(event)
 {
@@ -359,7 +387,7 @@ $(document).on('click', '#card_list li', function(event)
       url : "http://" + lb_server + "/make_primary_creditcard/" + localStorage.getItem('id'),
       data : { current_creditcard : current_creditcard },
       success : function (data) {
-        
+
         $('#make_primary_creditcard').off('click');
         $.mobile.changePage('#credit_card_list');
       },
