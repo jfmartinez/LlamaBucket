72b71e340b3564e2d663cfad31a2b3beedae33b1
Jose F. Martinez <jose.martinez60@upr.edu>
2013-12-11 11:45:01 -0400

    Fixing registration page

--- js/registration.js
+++ js/registration.js
@@ -1,30 +1,34 @@
 
 //Register a user
-$(document).on('click', '#register_user', function (event) {
 
-  $.ajax({
+//   $.ajax({
     
-    type : "POST",
+//     type : "POST",
 
-    url : "http://" + lb_server + "/register_user",
+//     url : "http://" + lb_server + "/register_user",
 
-    data : $('#new_user').serializeArray(),
+//     data : $('#new_user').serializeArray(),
 
-    success : function (data) {
+//     success : function (data) {
 
-      //Store the user in localStorage
-      localStorage.setItem('id', data.id);
+//       //Store the user in localStorage
+//       localStorage.setItem('id', data.id);
 
-      $('#user_profile_trigger').trigger("click");
-    },
+//       $('#user_profile_trigger').trigger("click");
+//     },
 
-    error : function (data) {
-      console.log(data);
+//     error : function (data) {
+//       console.log(data);
 
-      $.mobile.changePage('#home')
-    }
-  });
-});
+//       $.mobile.changePage('#home')
+//     }
+//   });
+// });
+
+$(document).on('pagebeforeshow', '#registerForm', initialize_register_form);
+
+
+
 
 $(document).on('change', '#register_same_address', function(event) {
   
@@ -49,3 +53,148 @@ $(document).on('change', '#register_same_address', function(event) {
 
 
 });
+
+ document.addEventListener("deviceready",onDeviceReady,false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+      console.log("SUP");
+      pictureSource=navigator.camera.PictureSourceType;
+      destinationType=navigator.camera.DestinationType;
+    }
+
+
+function capture_profile_photo()
+{
+  console.log("CAPTURING PHOTO");
+
+      var options = {
+        quality: 45,
+        targetWidth: 1000,
+        targetHeight: 1000,
+        destinationType: Camera.DestinationType.FILE_URI,
+        encodingType: Camera.EncodingType.JPEG,
+        sourceType: Camera.PictureSourceType.CAMERA
+      };
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(preview_profile_photo, on_profile_fail, options);
+
+
+}
+
+function browse_profile_photo()
+{
+
+     var options = {
+      quality: 45,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      destinationType: Camera.DestinationType.FILE_URI,
+      encodingType: Camera.EncodingType.JPEG,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    navigator.camera.getPicture(preview_profile_photo, on_profile_fail, options);
+
+
+}
+
+
+
+function preview_profile_photo(imageURI)
+{
+  console.log("Preview Image");
+
+      $('#new_profile_image').attr('src', imageURI);
+      $('#new_profile_image')[0].style.display = 'block';
+      sessionStorage.setItem('profile_image', imageURI);
+
+
+}
+function on_profile_fail()
+{
+      console.log("FAILED TO START CAMERA");
+
+}
+
+
+
+function upload_registration_form()
+{ 
+  console.log("UPLOADING FORM");
+
+  var imageURI = sessionStorage.getItem('profile_image');
+
+  var options = new FileUploadOptions();
+  options.fileKey ="file";
+          options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+          options.mimeType="image/jpeg";
+
+  var server_name = lb_server + "/register_user";
+
+  var form_user = $('#new_user').serializeArray();
+  var params = {};
+
+  for(var i = 0; i < form_user.length; i++)
+  {
+
+      params[form_user[i].name] = form_user[i].value;
+  }
+
+  console.log(params);
+
+
+  options.params = params;
+  var ft = new FileTransfer();
+  $.mobile.showPageLoadingMsg();
+  console.log("upload ight crash");
+  console.log(options);
+  console.log("UPLOADING FUNCTION");
+
+  ft.upload(imageURI, encodeURI(server_name), registration_success, registration_fail, options);
+
+}
+function registration_success(r)
+{
+  console.log(r.response);
+
+      var regex = /[^0-9]+/g;
+
+      var user_id= r.response.replace(regex, "");
+      localStorage.setItem('id', user_id);
+      console.log(user_id);
+
+      $('#user_profile_trigger').trigger("click");
+      $.mobile.hidePageLoadingMsg();
+
+
+}
+function registration_fail(r)
+{
+      $.mobile.hidePageLoadingMsg(); 
+
+  console.log(r);
+
+    $.mobile.changePage('#home');
+
+
+}
+function initialize_register_form()
+{
+
+
+
+
+  $('#new_user')[0].reset();
+
+  $('#preview_image').attr('src', '');
+
+  console.log("INITIALIAZING REGISTER PAGE");
+
+  $('#new_profile_pic').unbind('click').bind('click', capture_profile_photo);
+  $('#new_browse_profile_pic').unbind('click').bind('click', browse_profile_photo);
+
+
+  $('#register_user').unbind('click').bind('click', upload_registration_form);
+}
